660be29216deb74684b04a3809da6299f5dbc60c
Stiven Alexis <stiven22monsalvo@gmail.com>
2023-11-06 15:34:36 -0300

    change

--- familia.pl
+++ familia.pl
@@ -9,16 +9,30 @@ lindo(marta).
 lindo(rita).
 madre(marta, brus).
 madre(rita, victor).
-madre(rita, vernica).
+madre(rita, veronica).
+casado()
 
 
 abuelos_o_nietos(Abuelo, Nieto) :- padre(Abuelo, Hijo), madre(Hijo, Nieto); padre(Abuelo, Hijo), padre(Hijo, Nieto).
+hermana(Hermano1,Hermano2):- madre(Madres,Hermano1), madre(Madres,Hermano2), Hermano1 \= Hermano2.
+hijo(Hijo,Madre):- madre(Madre,Hijo).
 
 
 :- initialization(main).
 main:- write('Quien es el abuelo de victor?'),
     abuelos_o_nietos(Abuelo, victor),nl,
     write(Abuelo),nl,
-    write('Quienes son los nietos'),nl,
+    write('Quienes son los nietos de marcelo?'),nl,
     bagof(Nieto,abuelos_o_nietos(marcelo,Nieto),Nietos),
-    write(Nietos).
+    write(Nietos),nl,
+    write('Quien es la hermana de victor?'),nl,
+    hermana(H,victor),
+    write(H),nl,
+    write('Quien es el hermano de veronica?'),
+    hermana(H2,veronica),nl,
+    write(H2),nl,
+    write('De quien es hija veronica?'),
+    hijo(veronica,Madre),nl,
+    write(Madre).
+    
+    
